ff68a45e5b277adcc254412eb125a2c0d49656c8
GonzaloDP <267784580+GonzaloDP@users.noreply.github.com>
2026-06-20 12:21:58 -0300

    Correción de bugs

--- videojuego_prototipado/scripts/healer.gd
+++ videojuego_prototipado/scripts/healer.gd
@@ -1,5 +1,6 @@
 extends StaticBody2D
 
+@onready var label_no_mony = $NoTeAlcanza
 @onready var label = $Label
 @onready var player = get_tree().get_first_node_in_group("player")
 @onready var confirmacion_mensage = $"Mensaje de confirmación"
@@ -8,11 +9,12 @@ var player_cerca = false
 var ui_open = false
 
 func _ready() -> void:
+	label_no_mony.visible = false
 	label.visible = false
 	confirmacion_mensage.hide()
 
 func _process(delta: float) -> void:
-	if player_cerca and Input.is_action_just_pressed("interact") and player.coins >= 2 and ui_open == false:
+	if player_cerca and Input.is_action_just_pressed("interact") and ui_open == false:
 		abrir()
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
@@ -28,8 +30,12 @@ func _on_area_2d_body_exited(body: Node2D) -> void:
 func _on_cancelar_pressed() -> void:
 	confirmacion_mensage.hide()
 	player.control_habilitado = true
+	ui_open = false
 
 func _on_aceptar_pressed() -> void:
+	if player.coins <= 1:
+		label_no_mony.visible = true
+		$NoTeAlcanza/tiempodevida.start()
 	if player.coins >= 2:
 		player.add_coin(-2)
 		player.health += 20
@@ -38,3 +44,6 @@ func abrir():
 	confirmacion_mensage.show()
 	ui_open = true
 	player.control_habilitado = false
+
+func _on_tiempodevida_timeout() -> void:
+	label_no_mony.visible = false
