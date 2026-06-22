b6c6aa76abbe4123ab747aa7ecddfe5972561536
GonzaloDP <267784580+GonzaloDP@users.noreply.github.com>
2026-06-22 12:51:38 -0300

    Animación de pantalla de muerte

--- videojuego_prototipado/scripts/player/states/die.gd
+++ videojuego_prototipado/scripts/player/states/die.gd
@@ -16,7 +16,8 @@ func enter():
 		else:
 			$"../../Sprite2D/AnimationPlayer".play("deadleft")
 	
-	timer = 1.8
+	mostrar_gameover()
+	timer = 3.0
 
 
 func update(delta):
@@ -24,3 +25,25 @@ func update(delta):
 	if timer <= 0.0:
 		entity.queue_free()
 		get_tree().change_scene_to_file("res://scenes/menu.tscn")
+
+func mostrar_gameover():
+	var panel = get_tree().get_first_node_in_group("gameover")
+	var fondo = panel.get_node("ColorRect")
+	var texto = panel.get_node("Label")
+	var tween = create_tween()
+	var camara = entity.get_node("Camera2D")
+	
+	texto.visible = true
+	
+	fondo.color.a = 0.0
+	texto.modulate.a = 0.0
+	texto.scale = Vector2(0.5,0.5)
+	
+	texto.pivot_offset = texto.size / 2
+	
+	tween.parallel().tween_property(fondo,"color:a",0.8,2.0)
+	tween.parallel().tween_property(texto,"modulate:a",1.0,1.5)
+	tween.parallel().tween_property(texto,"scale",Vector2(1.5,1.5),3.0)
+	
+	create_tween().tween_property(camara,"zoom",Vector2(3,3),3.0)
+	
